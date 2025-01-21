16ca24c0a75c711be9a99590d3fea43e353ed43c
anikpk <anikpkanik@gmail.com>
2025-01-21 16:18:21 +0600

    Logging setup - init

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-from src.mlProject.config import configuration as config
+# from src.mlProject.config import configuration as config
+
+
+from src.mlProject.logging import logger
+
+logger.infO("Checking this logger:")
